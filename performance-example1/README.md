a39700866624f805626f32c70b2eff06b66f6d21
Puneet Vashisht <puneetvashisht@gmail.com>
2024-02-20 17:30:55 +0530

    fkdd

--- performance-example1/README.md
+++ performance-example1/README.md
@@ -1,3 +1,19 @@
 ## Getting Started
     #Print Compilation Details
     java -XX:+PrintCompilation App 5000
+
+    #Tweak Code Cache Size to optimize program - use case: frequent calls to some methods resulting in increased code cache size
+    'C:\Program Files\Java\jdk1.8.0_202\bin\java.exe' -XX:ReservedCodeCacheSize=28m -XX:+PrintCodeCache App 50000
+    ReservedCodeCacheSize
+    InitialCodeCacheSize
+
+
+    Print all JVM flags with default values using : java -XX:+PrintFlagsFinal
+
+    Shortcut: jps-> get the processes
+    jinfo wid pid to get specific flag for that process
+    jinfo -flag <name_flag> pId
+
+
+
+
--- performance-example1/src/App.java
+++ performance-example1/src/App.java
@@ -1,9 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Prime numbers!");
+        // find time to run the program
+//        Thread.sleep(50000);
+        long startTime = System.currentTimeMillis();
+    
         PrimeNumbers primeNumbers = new PrimeNumbers();
         // args[0] is the first command line argument
         primeNumbers.generatePrimes(Integer.parseInt(args[0]));
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time to run: " + (endTime - startTime) + "ms");
+
         // primeNumbers.generatePrimes(10);
     }
 }
